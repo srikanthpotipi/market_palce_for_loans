e2f9407b389d1c929fd3d4830aea530414a952ab
srikanthpotipi <potipireddisrikanth@gmail.com>
2021-01-29 15:29:38 +0530

    Removing print statemets, adding few more comments

--- loan_market_place.py
+++ loan_market_place.py
@@ -1,3 +1,5 @@
+# Solution Author : Srikanth
+
 # data structure to combine the buyers particpation and percentage
 class Bid:
     def __init__(self,participation, percentage, index ):
@@ -43,11 +45,10 @@ def get_total_participation(*bids):
     return total
 
 # logic to give the indices to give max profit to the seller
-# TODO --> removed the repeted max comparison to a fuction
 def max_seller_profit(sorted_bids, loan_amount):
     result = []
     max_amount = 0.0
-    # outer loop
+    # OUTER loop
     for bid_index in range(0, len(sorted_bids)):
         # check if the single value matches the loan amount
         if sorted_bids[bid_index].participation == loan_amount:
@@ -55,13 +56,12 @@ def max_seller_profit(sorted_bids, loan_amount):
             if total > max_amount:
                 result = [sorted_bids[bid_index].index]
             max_amount = max(max_amount, total)
-            print("single value here tot: {}, max_value : {}, result : {}".format(total, max_amount, result)) 
 
         # left points to the START & right to the END
         left, right = bid_index + 1, len(sorted_bids) - 1
         target_amount = loan_amount - sorted_bids[bid_index].participation
 
-        # inner loop
+        # INNER loop
         while(left < right):
             # check if outer index & left matches loan amount
             if (sorted_bids[bid_index].participation + sorted_bids[left].participation) == target_amount:
@@ -69,7 +69,6 @@ def max_seller_profit(sorted_bids, loan_amount):
                 if total > max_amount:
                     result = [sorted_bids[bid_index].index, sorted_bids[left].index]
                 max_amount = max(max_amount, total)
-                print("bid left here tot: {}, max_value : {}, reult : {}".format(total, max_amount, result))
 
             # check if outer index & left matches loan amount    
             if (sorted_bids[bid_index].participation + sorted_bids[right].participation) == target_amount:
@@ -78,7 +77,6 @@ def max_seller_profit(sorted_bids, loan_amount):
                     # matches paticipation tha matches loan amount
                     result = [sorted_bids[bid_index].index, sorted_bids[right].index]
                 max_amount = max(max_amount, total)
-                print("bid right here tot: {}, max_value : {}, reult : {}".format(total, max_amount, result))
 
             current_amount = sorted_bids[left].participation + sorted_bids[right].participation
 
@@ -87,8 +85,7 @@ def max_seller_profit(sorted_bids, loan_amount):
                 total = get_total_participation(sorted_bids[left], sorted_bids[right])
                 if total > max_amount:
                     result = [sorted_bids[left].index, sorted_bids[right].index]
-                max_amount = max(max_amount, total)   
-                print("bid left right here tot: {}, max_value : {}, reult : {}".format(total, max_amount, result)) 
+                max_amount = max(max_amount, total)
 
             # start comparing from left and right
             if current_amount == target_amount:
@@ -96,7 +93,8 @@ def max_seller_profit(sorted_bids, loan_amount):
                 if total > max_amount:
                     result = [sorted_bids[bid_index].index, sorted_bids[left].index, sorted_bids[right].index]
                 max_amount = max(max_amount, total)
-                print("bid left right here tot: {}, max_value : {}, reult : {}".format(total, max_amount, result))
+            
+            # shift left or right 
             if target_amount > current_amount:
                 left += 1
             else:
@@ -113,7 +111,9 @@ if __name__ == "__main__":
     raw_percentages = input("Enter your percentages:\n")
 
     loan_amount = float(raw_loan_amount)
-    
+    if loan_amount is None:
+        print("Enter valid loan amount")
+        exit(1)    
     # get the sorted array of bid objects
     sorted_bids = construct_bids(raw_participations, raw_percentages)
     if sorted_bids is not None:
@@ -122,5 +122,3 @@ if __name__ == "__main__":
         print("Enter valid particiaptions and percentages")
         exit(1)
 
-
-    
